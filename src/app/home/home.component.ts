7dc3b7dc072b8607aae2114cb2723b600d794b74
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-18 13:57:54 +0000

    create home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  template: ` <p>Home component</p> `,
+  styleUrl: './home.component.css',
+})
+
+export class HomeComponent {}
