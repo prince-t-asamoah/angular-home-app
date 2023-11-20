3070e4d7f32053783d313d33e2a9874c4cb64df1
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-20 11:05:54 +0000

    loop through housing dummy data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../@types/housing-location';
+import listings from '../data/listings';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +16,15 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
         <button type="button" class="primary">Search</button>
       </form>
     </section>
-    <section>
-      <app-housing-location></app-housing-location>
+    <section class="listings">
+      <app-housing-location
+        *ngFor="let housingLocation of housingLocationList"
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
     </section>
   `,
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = listings;
+}
