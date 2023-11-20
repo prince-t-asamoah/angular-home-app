9bde147432abc5c29331ac7ccb53ab67055866ca
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-20 12:55:11 +0000

    add details page route config

--- src/app/routes/config.ts
+++ src/app/routes/config.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
+import { DetailsComponent } from '../details/details.component';
 
 const routeConfig: Routes = [
-    {
-        path: '',
-        component: HomeComponent,
-        title: 'Homepage'
-    }
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Homepage',
+  },
+  {
+    path: 'details',
+    component: DetailsComponent,
+    title: 'Homepage',
+  },
 ];
 
 export default routeConfig;
