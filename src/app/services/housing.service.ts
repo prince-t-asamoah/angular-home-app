60699a1b5c023477ec17b7837887ef73148cb43c
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-21 16:43:28 +0000

    update getAllHousingLocations and getHousingLocationById method to use listing json database

--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -7,16 +7,20 @@ import listings from '../data/listings';
 })
 export class HousingService {
   protected housingLocationList: HousingLocation[] = listings;
+  url = 'http://localhost:3000/locations';
+
   constructor() {}
 
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return (await data.json()) ?? [];
   }
 
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(
-      (housingLocation) => housingLocation.id === id
-    );
+  async getHousingLocationById(
+    id: number
+  ): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return (await data.json()) ?? {};
   }
 
   submitApplication(firstName: string, lastName: string, email: string) {
