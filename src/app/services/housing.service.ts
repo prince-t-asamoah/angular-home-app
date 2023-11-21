473f0a0ab8e389a090dfd40459fba485bffd7814
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-21 08:54:51 +0000

    declare method to get all housing location

--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HousingLocation } from '../@types/housing-location';
+import listings from '../data/listings';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
-
+  protected housingLocationList: HousingLocation[] = listings;
   constructor() { }
+
+  getAllHousingLocations(): HousingLocation[] {
+    return this.housingLocationList;
+  }
 }
