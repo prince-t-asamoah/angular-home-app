f8f55f115807b8f7151182d7e021346398107561
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-21 09:57:18 +0000

    use get housing location by id service

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,19 +1,24 @@
+import { HousingService } from './../services/housing.service';
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingLocation } from '../@types/housing-location';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule],
-  template: ` <p>Details Page {{ housingLocationId }}</p> `,
+  template: ` <p>Details Page {{ housingLocation?.id }}</p> `,
   styleUrl: './details.component.css',
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
   }
 }
