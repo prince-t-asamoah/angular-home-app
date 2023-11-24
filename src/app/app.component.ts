5a3cbcf8fbcc4ccf0c1ca1924548ba6a850b8af4
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-24 10:10:55 +0000

    use tailwindcss classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,16 @@ import { HomeComponent } from './home/home.component';
   selector: 'app-root',
   standalone: true,
   imports: [HomeComponent, RouterModule],
-  template: ` <div class="app">
-    <header class="container">
-      <img
-        class="brand-logo"
-        src="/assets/logo.svg"
-        alt="logo"
-        aria-hidden="true"
-      />
+  template: ` <div class="min-h-screen">
+    <header class="px-10 py-5 sticky top-0 bg-white shadow-md">
+      <a routerLink="/" title="Home">
+        <img src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </a>
     </header>
-    <main class="container">
+    <main class="px-10">
       <router-outlet></router-outlet>
     </main>
   </div>`,
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Home App';
