0efd147e18aac62c400712d61d8f975a49b5e8dd
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-24 11:17:40 +0000

    setup app colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#605dc8",
+        "secondary-color": "#8b89e6",
+        "accent-color": "#e8e7fa",
+        "shadow-color": "#e8e8e8",
+      },
+    },
   },
   plugins: [],
 };
